22c5a36dd37bbb2df120a4c9f643489a92ec6a28
Amit <amitbiderman1@gmail.com>
2020-10-02 14:31:59 +0300

    Add files via upload

--- checkers/board.py
+++ checkers/board.py
@@ -13,7 +13,7 @@ class Board:
         win.fill(BLACK)
         for row in range(ROWS):
             for col in range(row % 2, COLS, 2):
-                pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col *SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                pygame.draw.rect(win, WHITE, (row*SQUARE_SIZE, col *SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
     def evaluate(self):
         return self.white_left - self.red_left + (self.white_kings * 0.5 - self.red_kings * 0.5)
