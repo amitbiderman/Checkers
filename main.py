f8ea642f7f3f21f080664d951e98610046a2309e
Amit <amitbiderman1@gmail.com>
2020-10-25 16:54:13 +0200

    Add files via upload
    Updated syntax

--- main.py
+++ main.py
@@ -8,39 +8,45 @@ FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Checkers')
 
-def get_row_col_from_mouse(pos):
-    x, y = pos
-    row = y // SQUARE_SIZE
-    col = x // SQUARE_SIZE
-    return row, col
 
-def main():
-    run = True
-    clock = pygame.time.Clock()
-    game = Game(WIN)
+class MainGame:
+    RUN = True
+    CLOCK = pygame.time.Clock()
+    GAME = Game(WIN)
 
-    while run:
-        clock.tick(FPS)
-        
-        if game.turn == WHITE:
-            value, new_board = minimax(game.get_board(), 4, WHITE, game)
-            game.ai_move(new_board)
+    @staticmethod
+    def get_row_col_from_mouse(pos):
+        x, y = pos
+        row = y // SQUARE_SIZE
+        col = x // SQUARE_SIZE
+        return row, col
 
-        if game.winner() != None:
-            print(game.winner())
-            run = False
+    def main(self):
+        while self.RUN:
+            self.CLOCK.tick(FPS)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-            
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                row, col = get_row_col_from_mouse(pos)
-                game.select(row, col)
+            if self.GAME.turn == WHITE:
+                value, new_board = minimax(self.GAME.get_board(), 2, WHITE, self.GAME)
+                self.GAME.ai_move(new_board)
 
-        game.update()
-    
-    pygame.quit()
+            if self.GAME.winner() is not None:
+                print(self.GAME.winner())
+                self.RUN = False
 
-main()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.RUN = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    row, col = self.get_row_col_from_mouse(pos)
+                    self.GAME.select(row, col)
+
+            self.GAME.update()
+
+        pygame.quit()
+
+
+if __name__ == '__main__':
+    maingame = MainGame()
+    maingame.main()
